21a0179d3b76766d7cde7b2755bde7b97b25d5df
admin <admin@YD-14>
2022-08-04 17:42:30 +0900

    200804

--- HelloWorld/src/homework/num.java
+++ HelloWorld/src/homework/num.java
@@ -6,10 +6,7 @@ public class num {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
 
-		int year = 0;
-		int age=0;
 		
-		System.out.println("년도를 입력하세요 ");
 		
 	}
 }
